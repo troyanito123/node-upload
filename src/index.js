178555e8b2c71a00f9d1c7f264baa998ffde5a26
jassmanni <jassmanniq@gmail.com>
2020-06-18 13:28:35 -0400

    remove uuid fron index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
-const uuid = require('uuid/dist/v4');
 const bodyParser = require('body-parser');
 const path = require('path');
 
@@ -23,8 +22,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 const storage = multer.diskStorage({
     destination: path.join(__dirname, 'public/img/uploads'),
     filename: (req, file, cb, filename) => {
-        console.log(file);
-        cb(null, uuid.default() + path.extname(file.originalname));
+        let name = new Date().getTime().toString()
+        cb(null, name + path.extname(file.originalname));
     }
 })
 app.use(multer({storage}).array('images', 5));
